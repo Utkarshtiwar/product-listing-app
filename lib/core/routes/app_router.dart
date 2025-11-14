10c3292bb6a8f41afe6be5415c7289c961c4a69a
Utkarshtiwar <utkarshtiwari15789@gmail.com>
2025-11-14 15:07:15 +0530

    added login screen route

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,12 +1,15 @@
 import 'package:get/get.dart';
 import '../../data/model/product_model.dart';
 import '../../presentation/screen/Cart_screen.dart';
+import '../../presentation/screen/login_screen.dart';
 import '../../presentation/screen/product_detail_screen.dart';
 import '../../presentation/screen/product_list_screen.dart';
 class AppRouter {
   static const String productList = '/';
   static const String productDetail = '/detail';
   static const String cart = '/cart';
+  static const String splash = '/splash';
+  static const String login = '/login';
 
   static final routes = [
     GetPage(
@@ -30,6 +33,12 @@ class AppRouter {
         page: () => CartScreen(),
       transition: Transition.rightToLeftWithFade,
       transitionDuration: Duration(milliseconds: 500),
+    ),
+    GetPage(
+      name: login,
+      page: () => LoginScreen(),
+      transition: Transition.rightToLeftWithFade,
+      transitionDuration: Duration(milliseconds: 350)
     )
   ];
 }
